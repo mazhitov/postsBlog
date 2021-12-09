06d4cec34fd76dae62b2f683bdbe6212957a503d
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-12-09 13:42:00 +0600

    component toolbar was created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: ToolbarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
